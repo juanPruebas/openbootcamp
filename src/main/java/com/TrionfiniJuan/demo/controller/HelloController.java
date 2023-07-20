1fe27fd228a85a88eb8902035b2bdf0849db6bdd
Juan Pedro <juanpedro.developer@gmail.com>
2023-07-20 15:12:36 -0300

    deploy

--- src/main/java/com/TrionfiniJuan/demo/controller/HelloController.java
+++ src/main/java/com/TrionfiniJuan/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.TrionfiniJuan.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+
     /**
      *  localhost:8080/api/saludo
      * @return "Hola"
      */
+    @Value("${app.message}")
+    private String messageSaludo;
+
+
     @GetMapping("/saludo")
     public ResponseEntity<String> saludar(){
-        return ResponseEntity.ok("Hola");
+        return ResponseEntity.ok("Hola " + messageSaludo);
     }
 
 
